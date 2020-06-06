a5319c0d454b48680821fce7682ea17a5a4a0b59
Lee-Anne Bierbaum <180085@virtualwindow.co.za>
2020-06-06 10:49:58 +0200

    Implement Score functionality. Update Questions;

--- app/src/main/java/com/example/pandemicstrivia/GameFragment.kt
+++ app/src/main/java/com/example/pandemicstrivia/GameFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.example.pandemicstrivia.databinding.FragmentGameBinding
 
 /**
@@ -26,40 +29,51 @@ class GameFragment : Fragment() {
         val viewModel : GameViewModel by viewModels()
         val binding: FragmentGameBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
 
+        viewModel.score.observe(viewLifecycleOwner, Observer{ newScore ->
+            binding.scoreText.text = newScore.toString()
+
+        })
+
         viewModel.question.observe(viewLifecycleOwner, Observer{ newQuestion ->
             binding.questionText.text = newQuestion.question
-            val answerOne = binding.answerOne
-            val answerTwo = binding.answerTwo
-            val answerThree = binding.answerThree
-
-           // answerOne.removeAllViews()
+            val answerGroup = binding.radioGroup
+            answerGroup.removeAllViews()
 
             for ((index, answer) in newQuestion.answers.withIndex()){
-                val newButton = createAnswerBtn(answer, index)
-                //add view
+                val newRdb = createAnswerGroup(answer, index)
+                answerGroup.addView((newRdb))
 
             }
         })
 
-        //binding.Button.setOnClickListener{
-        // val id = binding.button.id
-        // Toast.makeText(context, "Current id: ${id}", Toast.LENGTH_SHORT).show()
-        // viewModel.updateQuestion(viewModel.currentQuestion.value ?: 0)
 
-        // }
+        binding.doneButton.setOnClickListener{ view : View ->
+            val id = binding.radioGroup.checkedRadioButtonId
+            viewModel.checkQuestion(id)
+            Toast.makeText(context, "Current score: ${viewModel.score.value}", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "Current question id: ${viewModel.currentQuestion.value}", Toast.LENGTH_SHORT).show()
+            //Toast.makeText(context, "Current id: $id", Toast.LENGTH_SHORT).show()
+                if (viewModel.currentQuestion.value!! < viewModel.totalQuestions.value!!-1){
+                    viewModel.updateQuestion(viewModel.currentQuestion.value ?: 0)
+                 } else {
+                    view.findNavController().navigate(R.id.action_gameFragment_to_scoreFragment)
+                }
+
+
+         }
 
         return  binding.root
     }
 
-    private fun createAnswerBtn(answer: String, id:Int): Button{
-        val button = Button(context)
-        button.layoutParams = LinearLayout.LayoutParams(
+    private fun createAnswerGroup(answer: String, id:Int): RadioButton{
+        val rdb = RadioButton(context)
+        rdb.layoutParams = LinearLayout.LayoutParams(
             ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
-        button.text = answer
-        button.id = id
-        return button;
+        rdb.text = answer
+        rdb.id = id
+        return rdb;
     }
 
 }
--- app/src/main/java/com/example/pandemicstrivia/GameViewModel.kt
+++ app/src/main/java/com/example/pandemicstrivia/GameViewModel.kt
@@ -9,31 +9,41 @@ class GameViewModel : ViewModel() {
 
     private val questions = listOf<Question>(
         Question("This is our first Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree"),
-        Question("This is our first Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree"),
-        Question("This is our first Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree"),
-        Question("This is our first Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree"),
-        Question("This is our first Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree")
+        Question("This is our second Question?", listOf("2answerOne", "2answerTwo", "answerThree"), "answerThree"),
+        Question("This is our third Question?", listOf("3answerOne", "3answerTwo", "answerThree"), "answerThree"),
+        Question("This is our fourth Question?", listOf("4answerOne", "4answerTwo", "answerThree"), "answerThree"),
+        Question("This is our fifth Question?", listOf("answerOne", "answerTwo", "answerThree"), "answerThree")
     )
 
     private val _question = MutableLiveData<Question>()
     private val _currentQuestion = MutableLiveData<Int>()
     private val _score = MutableLiveData<Int>()
+    private val _totalQuestions = MutableLiveData<Int>()
 
     init {
         _score.value = 0
         _question.value = questions[0]
         _currentQuestion.value = 0
+        _totalQuestions.value = 3
     }
 
     val question: LiveData<Question> = _question
     val currentQuestion: LiveData<Int> = _currentQuestion
+    val score: LiveData<Int> = _score
+    val totalQuestions: LiveData<Int> = _totalQuestions
 
     fun updateQuestion(index: Int){
         _question.value = questions[index.plus(1)]
         _currentQuestion.value = index.plus(1)
-        // implement check of answer
-        //parse in id of button clicked and check if it matches the correct answer
+
     }
 
+    fun checkQuestion(selectedAnswer: Int){
+        val correctAnswer: String? = question.value?.correctAnswer
+        val userAnswer: String? = question.value?.answers?.get(selectedAnswer)
+        if (userAnswer == correctAnswer){
+            _score.value = score.value?.plus(1)
+        }
+    }
 
 }
